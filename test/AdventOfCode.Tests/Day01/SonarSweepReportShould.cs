15a9723c441d9212f77eff5cadb03829ec42e491
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-07 23:08:18 +0100

    refactor: 2021 - Day 01

--- test/AdventOfCode.Tests/Day01/SonarSweepReportShould.cs
+++ test/AdventOfCode.Tests/Day01/SonarSweepReportShould.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Xunit;
 
 namespace AdventOfCode.Day01
@@ -8,109 +5,23 @@ namespace AdventOfCode.Day01
     public class SonarSweepReportShould
     {
         [Theory]
-        [InlineData("199\n200\n208\n210\n200\n207\n240\n269\n260\n263", 7)]
-        [InputFileData("Day01/input.txt", 1581)]
+        [InlineData("199\n200\n208\n210\n200\n207\n240\n269\n260\n263", 1, 7)]
+        [InputFileData("Day01/input.txt", 1, 1581)]
+        [InlineData("199\n200\n208\n210\n200\n207\n240\n269\n260\n263", 3, 5)]
+        [InputFileData("Day01/input.txt", 3, 1618)]
         public void How_many_measurements_are_larger_than_the_previous_measurement(
             string sonarSweepReportDescription,
+            int measurementWindowSize,
             int expectedLargerMeasurementCount)
         {
-            //When
-            var measurements = sonarSweepReportDescription.Split('\n').Select(ushort.Parse).ToArray();
+            // Given
+            var measurements = SonarSweepReport.GetMeasurements(sonarSweepReportDescription, measurementWindowSize);
 
-            var largerMeasurementCount = 0;
-            var previousMeasurement = measurements.First();
-            foreach (var measurement in measurements)
-            {
-                if (measurement > previousMeasurement)
-                    largerMeasurementCount++;
+            // When
+            var largerMeasurementCount = SonarSweepReport.CountLargerMeasurements(measurements);
 
-                previousMeasurement = measurement;
-            }
-
-            //Then
+            // Then
             Assert.Equal(expectedLargerMeasurementCount, largerMeasurementCount);
-        }
-
-        [Theory]
-        [InlineData("199\n200\n208\n210\n200\n207\n240\n269\n260\n263", 5)]
-        [InputFileData("Day01/input.txt", 1618)]
-        public void How_many_measurements_are_larger_than_the_previous_sliding_windows_measurement(
-            string sonarSweepReportDescription,
-            int expectedLargerMeasurementCount)
-        {
-            //When
-            var measurementValues = sonarSweepReportDescription.Split('\n').Select(ushort.Parse).ToArray();
-
-            var measurements = measurementValues.Window(3).Select(w => new Measurement(w)).ToList();
-
-            var largerMeasurementCount = 0;
-            var previousMeasurement = measurements.First();
-            foreach (var measurement in measurements)
-            {
-                if (measurement > previousMeasurement)
-                    largerMeasurementCount++;
-
-                previousMeasurement = measurement;
-            }
-
-            //Then
-            Assert.Equal(expectedLargerMeasurementCount, largerMeasurementCount);
-        }
-    }
-
-    public record Measurement
-    {
-        private readonly IEnumerable<ushort> values;
-
-        public Measurement(IEnumerable<ushort> values)
-            => this.values = values;
-
-        public static bool operator >(Measurement a, Measurement b)
-            => a.values.Sum(v => v) > b.values.Sum(v => v);
-
-        public static bool operator <(Measurement a, Measurement b)
-            => a.values.Sum(v => v) < b.values.Sum(v => v);
-    }
-
-    public static class EnumerableExtensions
-    {
-        public static IEnumerable<IList<TSource>> Window<TSource>(this IEnumerable<TSource> source, int size)
-        {
-            if (source == null)
-                throw new ArgumentNullException(nameof(source));
-            if (size <= 0)
-                throw new ArgumentOutOfRangeException(nameof(size));
-
-            return _();
-
-            IEnumerable<IList<TSource>> _()
-            {
-                using var iter = source.GetEnumerator();
-
-                // generate the first window of items
-                var window = new TSource[size];
-                int i;
-                for (i = 0; i < size && iter.MoveNext(); i++)
-                    window[i] = iter.Current;
-
-                if (i < size)
-                    yield break;
-
-                while (iter.MoveNext())
-                {
-                    // generate the next window by shifting forward by one item
-                    // and do that before exposing the data
-                    var newWindow = new TSource[size];
-                    Array.Copy(window, 1, newWindow, 0, size - 1);
-                    newWindow[size - 1] = iter.Current;
-
-                    yield return window;
-                    window = newWindow;
-                }
-
-                // return the last window.
-                yield return window;
-            }
         }
     }
 }
