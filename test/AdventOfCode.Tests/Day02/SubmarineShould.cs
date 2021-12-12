ba424d5494501037de5428e2aa49e40d303f6b66
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-12 19:46:23 +0100

    feat: 2021 - Day 02 - Part One - Down command

--- test/AdventOfCode.Tests/Day02/SubmarineShould.cs
+++ test/AdventOfCode.Tests/Day02/SubmarineShould.cs
@@ -60,10 +60,36 @@ namespace AdventOfCode.Day02
             var actualPosition = submarine.Position;
             actualPosition.Should().Be(expectedPosition);
         }
+
+        [Theory]
+        [InlineData(5, 0, 5, 5, 5)]
+        [InlineData(13, 2, 8, 13, 10)]
+        public void Increases_the_depth_position_by_number_of_unit_from_down_command(
+            int startHorizontal,
+            int startDepth,
+            int commandUnit,
+            int endHorizontal,
+            int endDepth)
+        {
+            // Given
+            var expectedPosition = new Position(endHorizontal, endDepth);
+            var submarine = new Submarine(startHorizontal, startDepth);
+
+            // When
+            submarine.Execute(new DownCommand(commandUnit));
+
+            // Then
+            var actualPosition = submarine.Position;
+            actualPosition.Should().Be(expectedPosition);
+        }
     }
 
 
-    public record ForwardCommand(int Unit);
+    public record SubmarineCommand(int Unit);
+
+    public record DownCommand(int Unit) : SubmarineCommand(Unit);
+
+    public record ForwardCommand(int Unit) : SubmarineCommand(Unit);
 
     public class Submarine
     {
@@ -78,6 +104,9 @@ namespace AdventOfCode.Day02
 
         public void Execute(ForwardCommand command)
             => Position = Position with { Horizontal = Position.Horizontal + command.Unit };
+
+        public void Execute(DownCommand command)
+            => Position = Position with { Depth = Position.Depth + command.Unit };
     }
 
     public record Position(int Horizontal, int Depth);
