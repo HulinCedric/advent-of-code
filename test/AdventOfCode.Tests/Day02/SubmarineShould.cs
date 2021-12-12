6d5b5ac33c76ab2db2ec94c2620c21b018c7e5ea
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-12 19:43:28 +0100

    feat: 2021 - Day 02 - Part One - Submarine indiacted position

--- test/AdventOfCode.Tests/Day02/SubmarineShould.cs
+++ test/AdventOfCode.Tests/Day02/SubmarineShould.cs
@@ -18,12 +18,36 @@ namespace AdventOfCode.Day02
             // Then
             actualPosition.Should().Be(expectedPosition);
         }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(5, 10)]
+        public void Start_at_position_indicated(
+            int horizontal,
+            int depth)
+
+        {
+            // Given
+            var expectedPosition = new Position(horizontal, depth);
+            var submarine = new Submarine(horizontal, depth);
+
+            // When
+            var actualPosition = submarine.Position;
+
+            // Then
+            actualPosition.Should().Be(expectedPosition);
+        }
     }
+
 
     public class Submarine
     {
-        public Submarine()
-            => Position = new Position(0, 0);
+        public Submarine(int horizontal, int depth)
+            => Position = new Position(horizontal, depth);
+
+        public Submarine() : this(0, 0)
+        {
+        }
 
         public Position Position { get; }
     }
