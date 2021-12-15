59e5b4a10ca30f0e053bae77c14e324209b27c75
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-15 20:26:27 +0100

    feat: 2021 - Day 04 - Part One - Bingo board win on row

--- test/AdventOfCode.Tests/Day04/BoardShould.cs
+++ test/AdventOfCode.Tests/Day04/BoardShould.cs
@@ -30,6 +30,29 @@ namespace AdventOfCode.Day04
             // Then
             markedNumbers.Should().Contain(expectedDrawNumber);
         }
+
+        [Theory]
+        [InlineData(
+            "14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7",
+            new[] { 14, 21, 17, 24, 4 })]
+        [InlineData(
+            "14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7",
+            new[] { 18, 8, 23, 26, 20 })]
+        public void Win_when_all_number_of_a_row_is_marked(
+            string boardRepresentation,
+            int[] drawNumbers)
+        {
+            // Given
+            var bingoBoard = BingoBoardFactory.CreateWithRepresentation(boardRepresentation);
+            foreach (var drawNumber in drawNumbers)
+                bingoBoard.Mark(drawNumber);
+
+            // When
+            var hasWon = bingoBoard.HasWon;
+
+            // Then
+            hasWon.Should().BeTrue();
+        }
     }
 
     public static class BingoBoardFactory
@@ -53,6 +76,9 @@ namespace AdventOfCode.Day04
             => this.numbers = numbers
                    .Select(rowNumbers => rowNumbers.Select(number => new BoardNumber(number)).ToArray())
                    .ToArray();
+
+        public bool HasWon
+            => this.numbers.Any(rowNumbers => rowNumbers.All(number => number.IsMarked));
 
         public IEnumerable<int> MarkedNumbers
             => numbers.SelectMany(rowNumbers => rowNumbers)
