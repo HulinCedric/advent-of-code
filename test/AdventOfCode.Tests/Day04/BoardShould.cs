3cde9f70d32446d925f207245afb272e41ef606c
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-15 20:49:57 +0100

    feat: 2021 - Day 04 - Part One - Bingo board win on column

--- test/AdventOfCode.Tests/Day04/BoardShould.cs
+++ test/AdventOfCode.Tests/Day04/BoardShould.cs
@@ -53,6 +53,29 @@ namespace AdventOfCode.Day04
             // Then
             hasWon.Should().BeTrue();
         }
+
+        [Theory]
+        [InlineData(
+            "14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7",
+            new[] { 14, 10, 18, 22, 2 })]
+        [InlineData(
+            "14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7",
+            new[] { 17, 15, 23, 13, 12 })]
+        public void Win_when_all_number_of_a_column_is_marked(
+            string boardRepresentation,
+            int[] drawNumbers)
+        {
+            // Given
+            var bingoBoard = BingoBoardFactory.CreateWithRepresentation(boardRepresentation);
+            foreach (var drawNumber in drawNumbers)
+                bingoBoard.Mark(drawNumber);
+
+            // When
+            var hasWon = bingoBoard.HasWon;
+
+            // Then
+            hasWon.Should().BeTrue();
+        }
     }
 
     public static class BingoBoardFactory
@@ -78,12 +101,27 @@ namespace AdventOfCode.Day04
                    .ToArray();
 
         public bool HasWon
-            => this.numbers.Any(rowNumbers => rowNumbers.All(number => number.IsMarked));
+            => ContainWinningSet(NumbersByRow) || ContainWinningSet(NumbersByColumn);
 
         public IEnumerable<int> MarkedNumbers
             => numbers.SelectMany(rowNumbers => rowNumbers)
                 .Where(number => number.IsMarked)
                 .Select(number => number.Value);
+
+        private IEnumerable<IEnumerable<BoardNumber>> NumbersByColumn
+            => numbers.SelectMany(
+                    (rowNumbers, rowIndex) => rowNumbers
+                        .Select((number, columnIndex) => new { number, columnIndex, rowIndex }))
+                .GroupBy(flatten => flatten.columnIndex)
+                .Select(
+                    numbersGroupedByColumn => numbersGroupedByColumn
+                        .Select(flatten => flatten.number));
+
+        private IEnumerable<IEnumerable<BoardNumber>> NumbersByRow
+            => numbers;
+
+        private bool ContainWinningSet(IEnumerable<IEnumerable<BoardNumber>> numberSet)
+            => numberSet.Any(rowNumbers => rowNumbers.All(number => number.IsMarked));
 
         public void Mark(int drawNumber)
         {
