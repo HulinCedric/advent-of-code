7b8a1ffa0cf38eec563e20182238c0ce141de6d2
Cédric Hulin <hulin.cedric@gmail.com>
2021-12-15 22:01:30 +0100

    feat: 2021 - Day 04 - Part One - Bingo board winning score

--- test/AdventOfCode.Tests/Day04/BoardShould.cs
+++ test/AdventOfCode.Tests/Day04/BoardShould.cs
@@ -76,6 +76,31 @@ namespace AdventOfCode.Day04
             // Then
             hasWon.Should().BeTrue();
         }
+
+        [Theory]
+        [InlineData(
+            "14 21 17 24  4\n10 16 15  9 19\n18  8 23 26 20\n22 11 13  6  5\n 2  0 12  3  7",
+            new[] { 7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24 },
+            4512)]
+        public void
+            Provide_winning_score_with_the_sum_of_all_unmarked_numbers_multiply_by_the_last_number_permit_to_win(
+                string boardRepresentation,
+                int[] drawNumbers,
+                int expectedWinningScore)
+        {
+            // Given
+            var bingoBoard = BingoBoardFactory.CreateWithRepresentation(boardRepresentation);
+            foreach (var drawNumber in drawNumbers)
+                bingoBoard.Mark(drawNumber);
+
+            // When
+            var hasWon = bingoBoard.HasWon;
+            var winningScore = bingoBoard.WinningScore;
+
+            // Then
+            hasWon.Should().BeTrue();
+            winningScore.Should().Be(expectedWinningScore);
+        }
     }
 
     public static class BingoBoardFactory
@@ -94,11 +119,15 @@ namespace AdventOfCode.Day04
     public class BingoBoard
     {
         private readonly BoardNumber[][] numbers;
+        private int? numberPermitToWin;
 
         public BingoBoard(IEnumerable<int[]> numbers)
             => this.numbers = numbers
                    .Select(rowNumbers => rowNumbers.Select(number => new BoardNumber(number)).ToArray())
                    .ToArray();
+
+        public bool HasNotWon
+            => !HasWon;
 
         public bool HasWon
             => ContainWinningSet(NumbersByRow) || ContainWinningSet(NumbersByColumn);
@@ -120,6 +149,18 @@ namespace AdventOfCode.Day04
         private IEnumerable<IEnumerable<BoardNumber>> NumbersByRow
             => numbers;
 
+        public int WinningScore
+            => numberPermitToWin.HasValue ? ComputeWinningScore(numberPermitToWin.Value) : 0;
+
+        private int ComputeWinningScore(int winNumber)
+        {
+            var sumOfUnmarkedNumbers = numbers.SelectMany(rowNumbers => rowNumbers)
+                .Where(number => number.IsUnmarked)
+                .Sum(number => number.Value);
+
+            return sumOfUnmarkedNumbers * winNumber;
+        }
+
         private bool ContainWinningSet(IEnumerable<IEnumerable<BoardNumber>> numberSet)
             => numberSet.Any(rowNumbers => rowNumbers.All(number => number.IsMarked));
 
@@ -129,11 +170,17 @@ namespace AdventOfCode.Day04
             foreach (var number in rowNumbers)
                 if (number.Value == drawNumber)
                     number.IsMarked = true;
+
+            if (HasWon && !numberPermitToWin.HasValue)
+                numberPermitToWin = drawNumber;
         }
 
         private record BoardNumber(int Value)
         {
             internal bool IsMarked { get; set; }
+
+            internal bool IsUnmarked
+                => !IsMarked;
         }
     }
 }
