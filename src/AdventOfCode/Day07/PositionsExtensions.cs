2a11295449eaf8bee06bd0e007326f4388e555fe
Cédric Hulin <hulin.cedric@gmail.com>
2023-07-04 20:00:32 +0200

    feat: 2021 - Day 07 - Part One - Solution

---
+++ src/AdventOfCode/Day07/PositionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Day07;
+
+public static class PositionsExtensions
+{
+    public static int Median(this IEnumerable<int> positions)
+        => positions.OrderBy(position => position).ElementAt(positions.Count() / 2);
+
+    public static long GetCheapestTotalFuelCost(IEnumerable<int> positions)
+    {
+        // Compute total distance between each position and the chosen position
+        var chosenPosition = positions.Median();
+
+        var totalFuelCost = positions.Sum(position => Math.Abs(position - chosenPosition));
+
+        return totalFuelCost;
+    }
+}
