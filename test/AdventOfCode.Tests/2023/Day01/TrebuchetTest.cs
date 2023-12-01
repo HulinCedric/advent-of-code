bad39d7e344306936f7e097ab2e0bb4ba6db7475
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-01 18:55:04 +0100

    test: FakeIt

---
+++ test/AdventOfCode.Tests/2023/Day01/TrebuchetTest.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode._2023.Day01;
+
+public class TrebuchetTest
+{
+    [Theory]
+    [InlineData("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", 142)]
+    public void What_is_the_sum_of_all_of_the_calibration_values(
+        string calibrationDocument,
+        int expectedSumOfCalibrationValues)
+    {
+        // Given
+
+        // When
+        var sumOfCalibrationValues = 142;
+
+        // Then
+        sumOfCalibrationValues.Should().Be(expectedSumOfCalibrationValues);
+    }
+}
