721184fd00ec2ef609b6d3f9dfe88412b98ef0c8
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-02 17:04:53 +0100

    WIP

--- test/AdventOfCode.Tests/2023/Day02/GameTest.cs
+++ test/AdventOfCode.Tests/2023/Day02/GameTest.cs
@@ -16,21 +16,39 @@ public class GamesTest
     [InputFileData("2023/Day02/sample.txt", 8)]
     public void What_is_the_sum_of_the_ids_of_those_games(
         string gamesInformation,
-        int expectedSumOfPossibleGames)
+        int expectedSumOfPossibleGameIds)
     {
         // Given
+        var games = Games.Parse(gamesInformation);
 
         // When
-        var sumOfPossibleGames = 8;
+        var sumOfPossibleGameIds = games.Values
+            .Where(game => game.IsPossible())
+            .Sum(game => game.Id);
 
         // Then
-        sumOfPossibleGames.Should().Be(expectedSumOfPossibleGames);
+        sumOfPossibleGameIds.Should().Be(expectedSumOfPossibleGameIds);
     }
 
     [Theory]
     [InputFileData("2023/Day02/sample.txt")]
     public Task Parse_games(string gamesInformation)
         => Verify(Games.Parse(gamesInformation));
+
+    [Theory]
+    [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true)]
+    [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true)]
+    [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false)]
+    [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", false)]
+    [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", true)]
+    public void Is_valid(string gameInformation, bool isPossible)
+    {
+        // Given
+        var game = Game.Parse(gameInformation);
+
+        // Then
+        game.IsPossible().Should().Be(isPossible);
+    }
 }
 
 public class Games
@@ -66,6 +84,9 @@ public record Game
 
         return new Game(id, hands);
     }
+
+    public bool IsPossible()
+        => CubeSets.Values.All(x => x.Cubes.All(c => c.IsPossible()));
 }
 
 public class CubeSets
@@ -117,5 +138,19 @@ public class Cubes
         var parts = cubeHandInformation.Split(" ", StringSplitOptions.TrimEntries);
 
         return new Cubes(int.Parse(parts[0]), parts[1]);
+    }
+
+    public bool IsPossible()
+    {
+        if (Color == "red")
+            return Count <= 12;
+
+        if (Color == "green")
+            return Count <= 13;
+
+        if (Color == "blue")
+            return Count <= 14;
+
+        return false;
     }
 }
