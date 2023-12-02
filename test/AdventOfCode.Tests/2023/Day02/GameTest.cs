a40b88e01e2941b5c158c40a04d4f477ace71b9b
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-02 23:21:27 +0100

    Simplify arborescence

--- test/AdventOfCode.Tests/2023/Day02/GameTest.cs
+++ test/AdventOfCode.Tests/2023/Day02/GameTest.cs
@@ -1,15 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using FluentAssertions;
-using VerifyXunit;
 using Xunit;
-using static VerifyXunit.Verifier;
 
 namespace AdventOfCode._2023.Day02;
 
-[UsesVerify]
 public class GamesTest
 {
     [Theory]
@@ -20,10 +16,10 @@ public class GamesTest
         int expectedSumOfPossibleGameIds)
     {
         // Given
-        var games = Games.Parse(gamesInformation);
+        var games = Game.ParseMany(gamesInformation);
 
         // When
-        var sumOfPossibleGameIds = games.Values
+        var sumOfPossibleGameIds = games
             .Where(game => game.IsPossible())
             .Sum(game => game.Id);
 
@@ -39,10 +35,10 @@ public class GamesTest
         int expectedSumOfPossibleGameIds)
     {
         // Given
-        var games = Games.Parse(gamesInformation);
+        var games = Game.ParseMany(gamesInformation);
 
         // When
-        var sumOfPossibleGameIds = games.Values
+        var sumOfPossibleGameIds = games
             .Sum(game => game.Power());
 
         // Then
@@ -50,17 +46,12 @@ public class GamesTest
     }
 
     [Theory]
-    [InputFileData("2023/Day02/sample.txt")]
-    public Task Parse_games(string gamesInformation)
-        => Verify(Games.Parse(gamesInformation));
-
-    [Theory]
     [InlineData("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", true)]
     [InlineData("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", true)]
     [InlineData("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", false)]
     [InlineData("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", false)]
     [InlineData("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", true)]
-    public void Is_valid(string gameInformation, bool isPossible)
+    public void Is_Possible(string gameInformation, bool isPossible)
     {
         // Given
         var game = Game.Parse(gameInformation);
@@ -70,28 +61,20 @@ public class GamesTest
     }
 }
 
-public class Games
-{
-    private Games(List<Game> values)
-        => Values = values;
-
-    public List<Game> Values { get; }
-
-    public static Games Parse(string gamesInformation)
-        => new(gamesInformation.Split('\n').Select(Game.Parse).ToList());
-}
-
 public record Game
 {
-    private Game(int id, Hands hands)
+    private Game(int id, List<Hand> hands)
     {
         Id = id;
         Hands = hands;
     }
 
-    public Hands Hands { get; }
+    public List<Hand> Hands { get; }
 
     public int Id { get; }
+
+    public static List<Game> ParseMany(string gamesInformation)
+        => gamesInformation.Split('\n').Select(Parse).ToList();
 
     public static Game Parse(string gameInformation)
     {
@@ -99,42 +82,25 @@ public record Game
 
         var id = int.Parse(gameInformationParts[0].Replace("Game ", ""));
 
-        var hands = Hands.Parse(gameInformationParts[1]);
+        var hands = Hand.ParseMany(gameInformationParts[1]);
 
         return new Game(id, hands);
     }
 
     public bool IsPossible()
-        => Hands.Values.All(x => x.Cubes.All(c => c.IsPossible()));
+        => Hands.All(x => x.Cubes.All(c => c.IsPossible()));
 
     public int Power()
     {
         // max count by color
         var fewestNumberOfCubesByColor =
-            Hands.Values
+            Hands
                 .SelectMany(hand => hand.Cubes)
                 .GroupBy(cubes => cubes.Color)
                 .Select(g => g.Max(cubes => cubes.Count));
 
         // multiply all
         return fewestNumberOfCubesByColor.Aggregate(1, (current, next) => current * next);
-    }
-}
-
-public class Hands
-{
-    private Hands(List<Hand> values)
-        => Values = values;
-
-    public List<Hand> Values { get; }
-
-    public static Hands Parse(string handsInformation)
-    {
-        var cubesSets = handsInformation.Trim().Split(";", StringSplitOptions.TrimEntries);
-
-        var cubesSubSets = cubesSets.Select(Hand.Parse).ToList();
-
-        return new Hands(cubesSubSets);
     }
 }
 
@@ -145,13 +111,19 @@ public class Hand
 
     public List<Cubes> Cubes { get; }
 
-    public static Hand Parse(string s)
+    public static List<Hand> ParseMany(string handsInformation)
     {
-        var cubes = s.Split(",", StringSplitOptions.TrimEntries).Select(Day02.Cubes.Parse).ToList();
+        var cubesSets = handsInformation.Trim().Split(";", StringSplitOptions.TrimEntries);
 
-
-        return new Hand(cubes);
+        return cubesSets.Select(Parse).ToList();
     }
+
+    private static Hand Parse(string handInformation)
+        => new(
+            handInformation
+                .Split(",", StringSplitOptions.TrimEntries)
+                .Select(Day02.Cubes.Parse)
+                .ToList());
 }
 
 public class Cubes
@@ -173,16 +145,11 @@ public class Cubes
     }
 
     public bool IsPossible()
-    {
-        if (Color == "red")
-            return Count <= 12;
-
-        if (Color == "green")
-            return Count <= 13;
-
-        if (Color == "blue")
-            return Count <= 14;
-
-        return false;
-    }
+        => Color switch
+        {
+            "red" => Count <= 12,
+            "green" => Count <= 13,
+            "blue" => Count <= 14,
+            _ => false
+        };
 }
