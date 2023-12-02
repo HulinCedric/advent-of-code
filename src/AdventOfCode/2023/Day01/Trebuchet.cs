8ba762e907fa94dadcc3229f8435cc3b36502b55
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-02 09:42:05 +0100

    Complied regex Clearer

--- src/AdventOfCode/2023/Day01/Trebuchet.cs
+++ src/AdventOfCode/2023/Day01/Trebuchet.cs
@@ -4,8 +4,29 @@ using System.Text.RegularExpressions;
 
 namespace AdventOfCode._2023.Day01;
 
-public class Trebuchet
+public static partial class Trebuchet
 {
+    private static readonly Dictionary<string, string> SpelledOutDigitTranslation = new()
+    {
+        { "one", "1" },
+        { "two", "2" },
+        { "three", "3" },
+        { "four", "4" },
+        { "five", "5" },
+        { "six", "6" },
+        { "seven", "7" },
+        { "eight", "8" },
+        { "nine", "9" }
+    };
+
+    public static int FindCalibrationValue(string calibrationValueAmended)
+    {
+        var firstDigit = calibrationValueAmended.First(char.IsDigit);
+        var lastDigit = calibrationValueAmended.Last(char.IsDigit);
+
+        return int.Parse(string.Concat(firstDigit, lastDigit));
+    }
+
     public static int FindCalibrationValueWithSpelledOutDigit(string calibrationValueAmended)
     {
         var firstDigit = "";
@@ -47,33 +68,11 @@ public class Trebuchet
     }
 
     private static string ReplaceSpelledOutDigit(string calibrationValueAmended)
-    {
-        var spelledOutDigitTranslation = new Dictionary<string, string>
-        {
-            { "one", "1" },
-            { "two", "2" },
-            { "three", "3" },
-            { "four", "4" },
-            { "five", "5" },
-            { "six", "6" },
-            { "seven", "7" },
-            { "eight", "8" },
-            { "nine", "9" }
-        };
+        => SpelledOutDigitRegex()
+            .Replace(
+                calibrationValueAmended,
+                match => SpelledOutDigitTranslation[match.Value]);
 
-        const string pattern = "(one|two|three|four|five|six|seven|eight|nine)";
-
-        return Regex.Replace(
-            calibrationValueAmended,
-            pattern,
-            match => spelledOutDigitTranslation[match.Value]);
-    }
-
-    public static int FindCalibrationValue(string calibrationValueAmended)
-    {
-        var firstDigit = calibrationValueAmended.First(char.IsDigit);
-        var lastDigit = calibrationValueAmended.Last(char.IsDigit);
-
-        return int.Parse(string.Concat(firstDigit, lastDigit));
-    }
+    [GeneratedRegex("(one|two|three|four|five|six|seven|eight|nine)")]
+    private static partial Regex SpelledOutDigitRegex();
 }
