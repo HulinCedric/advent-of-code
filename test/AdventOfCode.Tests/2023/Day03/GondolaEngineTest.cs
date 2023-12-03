71b4a92019c94d10283c486acd9e52c56ee30f9b
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-03 21:14:34 +0100

    wip

--- test/AdventOfCode.Tests/2023/Day03/GondolaEngineTest.cs
+++ test/AdventOfCode.Tests/2023/Day03/GondolaEngineTest.cs
@@ -9,16 +9,48 @@ public class GondolaEngineTest
 {
     [Theory]
     [InputFileData("2023/Day03/sample.txt", 4361)]
-    public void Sum_of_all_part_numbers(string schematicDescription, int expectedSum)
+    [InputFileData("2023/Day03/input.txt", 538792)]
+    public void Sum_of_all_part_numbers(string schematic, int expectedSum)
     {
         // Arrange
-        var schematic = schematicDescription.Split('\n');
+        var map = schematic.Split('\n');
 
         // Act
-        var actualSum = GondolaEngine.CalculatePartNumbersSum(schematic);
+        var actualSum = GondolaEngine.CalculatePartNumbersSum(map);
 
         // Assert
         actualSum.Should().Be(expectedSum);
+    }
+
+    [Theory]
+    [InputFileData("2023/Day03/sample.txt", new[] { 467, 35, 633, 617, 592, 755, 664, 598 })]
+    public void All_part_numbers(string schematic, int[] expectedPartNumbers)
+    {
+        // Arrange
+        var map = schematic.Split('\n');
+
+        // Act
+        var partNumbers = GondolaEngine.ParseNumberInMap(map)
+            .Where(n => n.IsPartNumber(map))
+            .Select(n => n.Value);
+
+        // Assert
+        partNumbers.Should().BeEquivalentTo(expectedPartNumbers);
+    }
+
+    [Theory]
+    [InputFileData("2023/Day03/sample.txt", new[] { 467, 114, 35, 633, 617, 58, 592, 755, 664, 598 })]
+    public void All_numbers(string schematic, int[] expectedPartNumbers)
+    {
+        // Arrange
+        var map = schematic.Split('\n');
+
+        // Act
+        var numbers = GondolaEngine.ParseNumberInMap(map)
+            .Select(n => n.Value);
+
+        // Assert
+        numbers.Should().BeEquivalentTo(expectedPartNumbers);
     }
 
     [Theory]
@@ -30,7 +62,8 @@ public class GondolaEngineTest
     public void Identify_numbers(string schematic, int[] expectedNumber)
     {
         // Act
-        var number = GondolaEngine.GetNumbers(schematic);
+        var map = schematic.Split('\n');
+        var number = GondolaEngine.ParseNumberInMap(map);
 
         // Assert
         number.Select(n => n.Value).Should().BeEquivalentTo(expectedNumber);
@@ -40,54 +73,76 @@ public class GondolaEngineTest
     [InlineData(".114.\n.....")]
     [InlineData("467.\n...*")]
     [InlineData("..*.\n.35.\n....")]
+    [InlineData(".....\n.633.\n.#...")]
+    [InlineData("....\n617*\n....")]
     public void Parse_number_in_map(string schematic)
     {
+        // Arrange
+        var map = schematic.Split('\n');
+
         // Act
-        var numbers = GondolaEngine.GetNumbers(schematic);
+        var numbers = GondolaEngine.ParseNumberInMap(map);
 
         // Assert
         numbers.Should().HaveCount(1);
+    }
+
+    [Theory]
+    [InlineData(".114.\n.....", false)]
+    [InlineData("467.\n...*", true)]
+    [InlineData("..*.\n.35.\n....", true)]
+    [InlineData(".....\n.633.\n.#...", true)]
+    [InlineData("....\n617*\n....", true)]
+    [InlineData("....\n.58.\n....", false)]
+    [InlineData("....+\n.592.\n.....", true)]
+    [InlineData("...$.\n.664.", true)]
+    public void Is_part_number(string schematic, bool expectedIsPartNumber)
+    {
+        // Arrange
+        var map = schematic.Split('\n');
+        var number = GondolaEngine.ParseNumberInMap(map).Single();
+
+        // Act
+        var isPartNumber = number.IsPartNumber(map);
+
+        // Assert
+        isPartNumber.Should().Be(expectedIsPartNumber);
     }
 }
 
 public static class GondolaEngine
 {
-    public static int CalculatePartNumbersSum(string[] schematic)
-        => 4361;
+    public static int CalculatePartNumbersSum(string[] map)
+    {
+        var partNumbers = ParseNumberInMap(map);
+        return partNumbers.Where(n => n.IsPartNumber(map)).Sum(n => n.Value);
+    }
 
 
     private static PartNumber[] GetNumbersInLine(string schematicLine, int lineIndex)
     {
         var coordinates = new List<(int startX, int endX, int y)>();
         for (var columnIndex = 0; columnIndex < schematicLine.Length; columnIndex++)
-        {
             if (char.IsDigit(schematicLine[columnIndex]))
             {
                 var endX = columnIndex;
                 while (endX < schematicLine.Length &&
                        char.IsDigit(schematicLine[endX]))
-                {
                     endX++;
-                }
 
                 coordinates.Add((columnIndex, endX, lineIndex));
                 columnIndex = endX;
             }
-        }
 
         var numbers = new List<PartNumber>();
         foreach (var coordinate in coordinates)
-        {
             numbers.Add(PartNumber.ExtractNumber(schematicLine, coordinate));
-        }
 
         return numbers.ToArray();
     }
 
-    public static IEnumerable<PartNumber> GetNumbers(string schematic)
+    public static IEnumerable<PartNumber> ParseNumberInMap(string[] map)
     {
-        var map = schematic.Split('\n');
-
         var partNumbers = new List<PartNumber>();
         for (var lineIndex = 0; lineIndex < map.Length; lineIndex++)
         {
@@ -121,5 +176,20 @@ public class PartNumber
             number = number * 10 + (schematic[i] - '0');
 
         return new PartNumber(number, coordinate.startX, coordinate.endX, coordinate.y);
+    }
+
+    public bool IsPartNumber(string[] map)
+    {
+        for (var x = StartX - 1; x <= EndX + 1; x++)
+        for (var y = Y - 1; y <= Y + 1; y++)
+            if (x >= 0 &&
+                x < map[0].Length &&
+                y >= 0 &&
+                y < map.Length)
+                if (!char.IsDigit(map[y][x]) &&
+                    map[y][x] != '.')
+                    return true;
+
+        return false;
     }
 }
