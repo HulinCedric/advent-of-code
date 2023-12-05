c7c12358924350cb7b4f2532881d8425c70bca31
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-05 14:49:23 +0100

    Solution

--- test/AdventOfCode.Tests/2023/Day04/ScratchCardsTest.cs
+++ test/AdventOfCode.Tests/2023/Day04/ScratchCardsTest.cs
@@ -24,6 +24,21 @@ public class ScratchCardsTest
     }
 
     [Theory]
+    [InputFileData("2023/Day04/sample.txt", 30)]
+    [InputFileData("2023/Day04/input.txt", 8736438)]
+    public void Should_calculate_total_scratch_cards(string table, int expectedTotalCards)
+    {
+        // Arrange
+        var cards = Table.Parse(table).ToList();
+
+        // Act
+        var cardCount = ScratchCardProcessor.Process(cards);
+
+        // Assert
+        cardCount.Should().Be(expectedTotalCards);
+    }
+
+    [Theory]
     [InputFileData("2023/Day04/sample.txt")]
     public void Should_parse_all_scratch_cards_on_table(string table)
         => Table.Parse(table).Should().HaveCount(6);
@@ -49,6 +64,34 @@ public class ScratchCardsTest
         => new ScratchCard(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 }).GetPoints().Should().Be(8);
 }
 
+public static class ScratchCardProcessor
+{
+    public static int Process(List<ScratchCard> cards)
+        => Enumerable
+            .Empty<int>()
+            .Concat(OriginalCardsCount(cards))
+            .Concat(CopyCardsCount(cards))
+            .Sum();
+
+    private static int[] OriginalCardsCount(List<ScratchCard> cards)
+        => cards.Select(_ => 1).ToArray();
+
+    private static int[] CopyCardsCount(List<ScratchCard> cards)
+    {
+        var cardsCount = new int[cards.Count];
+        for (var i = 0; i < cards.Count; i++)
+        {
+            var numberOfMatches = cards[i].Matches();
+            for (var j = 1; j <= numberOfMatches && i + j < cards.Count; j++)
+            {
+                cardsCount[i + j] += cardsCount[i] + 1;
+            }
+        }
+
+        return cardsCount;
+    }
+}
+
 public class Table
 {
     public static IEnumerable<ScratchCard> Parse(string table)
@@ -58,17 +101,17 @@ public class Table
                 {
                     var cardParts = cardInformation.Split(':', '|');
 
-                    var winningNumbers = cardParts[1]
-                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                        .Select(int.Parse)
-                        .ToArray();
-                    var numberYouHave = cardParts[2]
-                        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                        .Select(int.Parse)
-                        .ToArray();
+                    var winningNumbers = ParseCardNumber(cardParts[1]);
+                    var numberYouHave = ParseCardNumber(cardParts[2]);
 
                     return new ScratchCard(winningNumbers, numberYouHave);
                 });
+
+    private static int[] ParseCardNumber(string cardPart)
+        => cardPart
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
 }
 
 public class ScratchCard
@@ -85,6 +128,6 @@ public class ScratchCard
     public int GetPoints()
         => (int)Math.Pow(2, Matches() - 1);
 
-    private int Matches()
+    public int Matches()
         => winningNumbers.Intersect(numbersYouHave).Count();
 }
