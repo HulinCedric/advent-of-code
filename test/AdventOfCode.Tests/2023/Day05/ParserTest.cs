1ef45a7ea33a7c29e3964f917ad6da38d1fa2ed7
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-09 23:20:19 +0100

    Naive solution
    But underperformant with the puzzle input

--- test/AdventOfCode.Tests/2023/Day05/ParserTest.cs
+++ test/AdventOfCode.Tests/2023/Day05/ParserTest.cs
@@ -19,6 +19,18 @@ public class ParserTest
 
         lowestLocation.Should().Be(expectedLowestLocation);
     }
+
+    [Theory]
+    [InputFileData("2023/Day05/sample.txt", 46)]
+    // [InputFileData("2023/Day05/input.txt", 510109797)]
+    public void part_two(string almanacInformation, long expectedLowestLocation)
+    {
+        var gardener = AlmanacParser.Parse_Two(almanacInformation);
+
+        var lowestLocation = gardener.GetLowestLocation();
+
+        lowestLocation.Should().Be(expectedLowestLocation);
+    }
 }
 
 public static class AlmanacParser
@@ -58,4 +70,30 @@ public static class AlmanacParser
         => lines.Select(line => line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray())
             .Select(values => new SeedConverter(values[0], values[1], values[2]))
             .ToList();
+
+    public static Almanac Parse_Two(string almanacInformation)
+    {
+        var almanacParts = almanacInformation.Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+
+        return new Almanac(ParseSeeds_Two(almanacParts[0]), ParseMaps(almanacParts.Skip(1)));
+    }
+
+    private static List<long> ParseSeeds_Two(string almanacSeedPart)
+    {
+        var seedParts = almanacSeedPart
+            .Split(new[] { ':', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+            .Skip(1)
+            .Select(long.Parse)
+            .ToList();
+
+        var seeds = new List<long>();
+        for (var i = 0; i < seedParts.Count; i += 2)
+        {
+            var start = seedParts[i];
+            var length = seedParts[i + 1];
+            seeds.AddRange(Enumerable.Range((int)start, (int)length).Select(x => (long)x));
+        }
+
+        return seeds;
+    }
 }
