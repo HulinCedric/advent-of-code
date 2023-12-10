852a01ae1a4b1e2b03b6185f9a2c807b4eef2af6
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-10 22:44:31 +0100

    Sol Sample

--- test/AdventOfCode.Tests/2023/Day06/Test.cs
+++ test/AdventOfCode.Tests/2023/Day06/Test.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using FluentAssertions;
 using FluentAssertions.Extensions;
 using Xunit;
@@ -10,34 +11,21 @@ public class Test
     [Theory]
     [InputFileData("2023/Day06/sample.txt", 288)]
     //[InputFileData("2023/Day06/input.txt", 288)]
-    public void todo(string info, int expected)
+    public void todoSample(string info, int expected)
     {
-        var actual = 288;
+        // Arrange
+        var races = new[]
+        {
+            new Race(7, 9),
+            new Race(15, 40),
+            new Race(30, 200)
+        };
 
-        actual.Should().Be(expected);
+        var waysToBeatTheRecord = races.Select(r => r.CalculateWaysToWin()).Aggregate(1, (a, b) => a * b);
+
+        waysToBeatTheRecord.Should().Be(expected);
     }
 
-    // toy boats
-    // Holding down the button **charges the boat**
-    // releasing the button **allows the boat to move**
-
-
-    // race
-    // the **time** allowed for a race
-    // the best **distance** ever recorded in that race
-
-    // - The first race lasts 7 milliseconds. The record distance in this race is 9 millimeters.
-    // - The second race lasts 15 milliseconds. The record distance in this race is 40 millimeters.
-    // - The third race lasts 30 milliseconds. The record distance in this race is 200 millimeters.
-
-
-    // First test
-    // Your toy boat has a starting speed of **zero millimeters per millisecond**. For each whole millisecond you spend at the
-    // beginning of the race holding down the button, the boat's speed increases by **one millimeter per millisecond**.
-
-
-    // Don't hold the button at all (that is, hold it for **`0` milliseconds**) at the start of the race.
-    // The boat won't move; it will have traveled **`0` millimeters** by the end of the race.
     [Theory]
     [InlineData(0, 0)]
     [InlineData(1, 1)]
@@ -46,8 +34,8 @@ public class Test
     [InlineData(4, 4)]
     [InlineData(5, 5)]
     [InlineData(6, 6)]
-    [InlineData(7, 7)]    
-    public void BoatSpeedIncreasesWithTimeHoldingButton(int holdTime, int speed)
+    [InlineData(7, 7)]
+    public void Boat_Speed_IncreasesWith_TimeHoldingButton_by_OneMillimeterPerMillisecond(int holdTime, int speed)
     {
         // Arrange
         var boat = new Boat();
@@ -58,11 +46,64 @@ public class Test
         // Assert
         boat.Speed.Should().Be(speed.MillimetersPerMillisecond());
     }
-    
-    
+
+    [Theory]
+    [InlineData(0, 7, 0)]
+    [InlineData(1, 6, 6)]
+    [InlineData(2, 5, 10)]
+    [InlineData(3, 4, 12)]
+    [InlineData(4, 3, 12)]
+    [InlineData(5, 2, 10)]
+    [InlineData(6, 1, 6)]
+    [InlineData(7, 0, 0)]
+    public void Boat_Move_IncreasesDistance(int holdTimeIn, int moveTimeIn, int distanceIn)
+    {
+        // Arrange
+        var boat = new Boat();
+        boat.HoldButton(holdTimeIn.Milliseconds());
+
+        // Act
+        boat.Move(moveTimeIn.Milliseconds());
+
+        // Assert
+        boat.Distance.Should().Be(distanceIn.Millimeters());
+    }
+
+
+    [Theory]
+    [InlineData(7, 9, 4)]
+    [InlineData(15, 40, 8)]
+    [InlineData(30, 200, 9)]
+    public void Race_CalculateWaysToWin_ReturnsCorrectNumber(int time, int distance, int waysToWin)
+        => new Race(time, distance)
+            .CalculateWaysToWin()
+            .Should()
+            .Be(waysToWin);
 }
 
-public record Race(int Time, int Distance);
+public record Race(int Time, int Distance)
+{
+    public int CalculateWaysToWin()
+    {
+        var waysToWin = 0;
+        for (var i = 0; i < Time; i++)
+        {
+            var boat = new Boat();
+            boat.HoldButton(i.Milliseconds());
+
+            var timeLeftForTheRace = Time - i;
+
+            boat.Move(timeLeftForTheRace.Milliseconds());
+
+            if (boat.Distance > Distance)
+            {
+                waysToWin++;
+            }
+        }
+
+        return waysToWin;
+    }
+}
 
 public class Boat
 {
@@ -72,17 +113,23 @@ public class Boat
         Distance = 0;
     }
 
-    public int Distance { get; }
+    public int Distance { get; private set; }
 
     public int Speed { get; private set; }
 
     public void HoldButton(TimeSpan time)
         // 1 millisecond = 1 millimeter per millisecond
         => Speed = (int)time.TotalMilliseconds;
+
+    public void Move(TimeSpan time)
+        => Distance += Speed * (int)time.TotalMilliseconds;
 }
 
 public static class LangExtensions
 {
     public static int MillimetersPerMillisecond(this int value)
+        => value;
+
+    public static int Millimeters(this int value)
         => value;
 }
