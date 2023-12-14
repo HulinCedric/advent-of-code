4c2db6a46669a0d249daa3af4e77339559dc629e
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-14 22:24:47 +0100

    Previous step

--- test/AdventOfCode.Tests/2023/Day09/PuzzleTest.cs
+++ test/AdventOfCode.Tests/2023/Day09/PuzzleTest.cs
@@ -49,6 +49,27 @@ public class PuzzleTest
     [InlineData(new[] { 0, 3, 6, 9, 12, 15 }, 18)]
     [InlineData(new[] { 1, 3, 6, 10, 15, 21 }, 28)]
     [InlineData(new[] { 10, 13, 16, 21, 30, 45 }, 68)]
-    public void Extrapolate_next_steps(int[] steps, int nextStep)
+    public void Extrapolate_next_step(int[] steps, int nextStep)
         => steps.ExtrapolateNextStep().Should().Be(nextStep);
+
+    [Theory]
+    [InlineData(new[] { 0, 0, 0 }, 0)]
+    [InlineData(new[] { 1, 1, 1, 1 }, 1)]
+    [InlineData(new[] { 6, 5, 4, 3, 2 }, 1)]
+    [InlineData(new[] { 15, 12, 9, 6, 3, 0 }, -3)]
+    [InlineData(new[] { 21, 15, 10, 6, 3, 1 }, 0)]
+    [InlineData(new[] { 45, 30, 21, 16, 13, 10 }, 5)]
+    public void Extrapolate_previous_step_is_extrapolate_next_step_with_reverse_value(int[] steps, int nextStep)
+        => steps.ExtrapolateNextStep().Should().Be(nextStep);
+    
+    
+    [Theory]
+    [InlineData(new[] { 0, 0, 0 }, 0)]
+    [InlineData(new[] { 1, 1, 1, 1 }, 1)]
+    [InlineData(new[] { 2, 3, 4, 5, 6 }, 1)]
+    [InlineData(new[] { 0, 3, 6, 9, 12, 15 }, -3)]
+    [InlineData(new[] { 1, 3, 6, 10, 15, 21 }, 0)]
+    [InlineData(new[] { 10, 13, 16, 21, 30, 45 }, 5)]
+    public void Extrapolate_previous_step(int[] steps, int nextStep)
+        => steps.ExtrapolatePreviousStep().Should().Be(nextStep);
 }
