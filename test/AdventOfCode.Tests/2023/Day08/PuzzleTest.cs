2cd3a01c23084f6141cf197655ae9bc69d8f5fc9
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-14 09:29:07 +0100

    Perfoming location

--- test/AdventOfCode.Tests/2023/Day08/PuzzleTest.cs
+++ test/AdventOfCode.Tests/2023/Day08/PuzzleTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using Xunit;
@@ -14,16 +15,11 @@ public class HauntedWastelandShould
     {
         // Arrange
         var (instructions, network) = MapParser.Parse(mapDocument);
+        var startingLocation = "AAA";
+        var endingLocation = "ZZZ";
 
         // Act
-        var location = "AAA";
-        var steps = 0;
-        while (location != "ZZZ")
-        {
-            var (left, right) = network[location];
-            location = instructions[steps % instructions.Length] == 'R' ? right : left;
-            steps++;
-        }
+        var steps = FindEndLocation(startingLocation, network, instructions, endingLocation);
 
         // Assert
         steps.Should().Be(expectedSteps);
@@ -31,30 +27,54 @@ public class HauntedWastelandShould
 
     [Theory]
     [InputFileData("2023/Day08/sample3.txt", 6)]
-    [InputFileData("2023/Day08/input.txt", 20221)]
+    [InputFileData("2023/Day08/input.txt", 14616363770447)]
     public void ReachZZZInExpectedSteps_two(string mapDocument, long expectedSteps)
     {
         // Arrange
         var (instructions, network) = MapParser.Parse(mapDocument);
 
         // Act
-        var locations = network.Keys.Where(location => location.EndsWith("A")).ToList();
+        var startingLocations = network.Keys.Where(location => location.EndsWith("A")).ToList();
 
+        var step = startingLocations
+            .Select(startingLocation => FindEndLocation(startingLocation, network, instructions, "Z"))
+            .Aggregate(1L, CommonEndSteps);
+
+        // Assert
+        step.Should().Be(expectedSteps);
+    }
+
+    private static long FindEndLocation(
+        string location,
+        IReadOnlyDictionary<string, (string, string)> network,
+        char[] instructions,
+        string endLocation)
+    {
         var steps = 0L;
-        while (!locations.All(location => location.EndsWith('Z')))
+        while (!location.EndsWith(endLocation))
         {
-            for (var index = 0; index < locations.Count; index++)
-            {
-                var location = locations[index];
-                var (left, right) = network[location];
-                var newLocation = instructions[steps % instructions.Length] == 'R' ? right : left;
-                locations[index] = newLocation;
-            }
-
+            var (left, right) = network[location];
+            location = instructions[steps % instructions.Length] == 'R' ? right : left;
             steps++;
         }
 
-        // Assert
-        steps.Should().Be(expectedSteps);
+        return steps;
     }
+
+    private static long CommonEndSteps(long a, long b)
+        => LeastCommonMultiple(a, b);
+
+    /// <summary>
+    ///     Calculates the least common multiple (LCM) of two numbers.
+    /// </summary>
+    /// <seealso href="https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor" />
+    private static long LeastCommonMultiple(long a, long b)
+        => a / GreatestCommonDivisor(a, b) * b;
+
+    /// <remarks>
+    ///     Recursive implementation of GCD
+    /// </remarks>
+    /// <seealso href="https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations" />
+    private static long GreatestCommonDivisor(long a, long b)
+        => b == 0 ? a : GreatestCommonDivisor(b, a % b);
 }
