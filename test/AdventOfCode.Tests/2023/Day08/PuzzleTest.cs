5eb4420aa63d1ee2b5c730b5b3b6f1f96b7892e7
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-14 09:39:32 +0100

    refacto

--- test/AdventOfCode.Tests/2023/Day08/PuzzleTest.cs
+++ test/AdventOfCode.Tests/2023/Day08/PuzzleTest.cs
@@ -8,50 +8,41 @@ namespace AdventOfCode._2023.Day08;
 public class HauntedWastelandShould
 {
     [Theory]
-    [InputFileData("2023/Day08/sample1.txt", 2)]
-    [InputFileData("2023/Day08/sample2.txt", 6)]
-    [InputFileData("2023/Day08/input.txt", 20221)]
-    public void ReachZZZInExpectedSteps(string mapDocument, int expectedSteps)
+    [InputFileData("2023/Day08/sample1.txt", "AAA", "ZZZ", 2)]
+    [InputFileData("2023/Day08/sample2.txt", "AAA", "ZZZ", 6)]
+    [InputFileData("2023/Day08/input.txt", "AAA", "ZZZ", 20221)]
+    [InputFileData("2023/Day08/sample3.txt", "A", "Z", 6)]
+    [InputFileData("2023/Day08/input.txt", "A", "Z", 14616363770447)]
+    public void Reach_ending_location_in_expected_steps(
+        string mapDocument,
+        string startingLocation,
+        string endingLocation,
+        long expectedSteps)
     {
         // Arrange
         var (instructions, network) = MapParser.Parse(mapDocument);
-        var startingLocation = "AAA";
-        var endingLocation = "ZZZ";
 
         // Act
-        var steps = FindEndLocation(startingLocation, network, instructions, endingLocation);
+
+        var steps = StartingLocations(startingLocation, network)
+            .Select(location => StepsToEndingLocation(location, instructions, network, endingLocation))
+            .Aggregate(1L, CommonEndSteps);
 
         // Assert
         steps.Should().Be(expectedSteps);
     }
 
-    [Theory]
-    [InputFileData("2023/Day08/sample3.txt", 6)]
-    [InputFileData("2023/Day08/input.txt", 14616363770447)]
-    public void ReachZZZInExpectedSteps_two(string mapDocument, long expectedSteps)
-    {
-        // Arrange
-        var (instructions, network) = MapParser.Parse(mapDocument);
+    private static List<string> StartingLocations(string startingLocation, Dictionary<string, (string, string)> network)
+        => network.Keys.Where(location => location.EndsWith(startingLocation)).ToList();
 
-        // Act
-        var startingLocations = network.Keys.Where(location => location.EndsWith("A")).ToList();
-
-        var step = startingLocations
-            .Select(startingLocation => FindEndLocation(startingLocation, network, instructions, "Z"))
-            .Aggregate(1L, CommonEndSteps);
-
-        // Assert
-        step.Should().Be(expectedSteps);
-    }
-
-    private static long FindEndLocation(
+    private static long StepsToEndingLocation(
         string location,
-        IReadOnlyDictionary<string, (string, string)> network,
         char[] instructions,
-        string endLocation)
+        IReadOnlyDictionary<string, (string, string)> network,
+        string endingLocation)
     {
         var steps = 0L;
-        while (!location.EndsWith(endLocation))
+        while (!location.EndsWith(endingLocation))
         {
             var (left, right) = network[location];
             location = instructions[steps % instructions.Length] == 'R' ? right : left;
@@ -62,18 +53,15 @@ public class HauntedWastelandShould
     }
 
     private static long CommonEndSteps(long a, long b)
-        => LeastCommonMultiple(a, b);
+        => MathUtils.LeastCommonMultiple(a, b);
+}
 
-    /// <summary>
-    ///     Calculates the least common multiple (LCM) of two numbers.
-    /// </summary>
+public static class MathUtils
+{
     /// <seealso href="https://en.wikipedia.org/wiki/Least_common_multiple#Using_the_greatest_common_divisor" />
-    private static long LeastCommonMultiple(long a, long b)
+    public static long LeastCommonMultiple(long a, long b)
         => a / GreatestCommonDivisor(a, b) * b;
 
-    /// <remarks>
-    ///     Recursive implementation of GCD
-    /// </remarks>
     /// <seealso href="https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations" />
     private static long GreatestCommonDivisor(long a, long b)
         => b == 0 ? a : GreatestCommonDivisor(b, a % b);
