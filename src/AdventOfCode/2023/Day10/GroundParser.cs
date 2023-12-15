e1f2bd79c9a095ad6b31b2232ba6ccbf14e734e0
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-15 09:17:29 +0100

    Parser

---
+++ src/AdventOfCode/2023/Day10/GroundParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2023.Day10;
+
+public static class GroundParser
+{
+    public static Dictionary<(int, int), char> ParseGround(this string ground)
+    {
+        var lines = ground.Split("\n");
+        return (
+                   from x in Enumerable.Range(0, lines.Length)
+                   from y in Enumerable.Range(0, lines[0].Length)
+                   let position = (x, y)
+                   let tile = lines[x][y]
+                   select new KeyValuePair<(int, int), char>(position, tile)
+               ).ToDictionary();
+    }
+}
