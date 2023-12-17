e1b23f79cca8515b6dd35eb41502f11fd9da4ea6
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-17 14:37:51 +0100

    test: setup

---
+++ test/AdventOfCode.Tests/2023/Day12/PuzzleTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode._2023.Day12;
+
+public class PuzzleTest
+{
+    [Theory]
+    [InlineData("???.### 1,1,3", 1)]
+    [InlineData(".??..??...?##. 1,1,3", 4)]
+    public void todo(
+        string springsRow,
+        int arrangements)
+        => arrangements.Should()
+            .Be(arrangements);
+}
