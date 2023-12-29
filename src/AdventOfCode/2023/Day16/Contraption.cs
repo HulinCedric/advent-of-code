f8249eb8d8c8b4e588ec2bc2819a5196c7b1cb35
Cédric Hulin <hulin.cedric@gmail.com>
2023-12-29 15:09:36 +0100

    Sol

--- src/AdventOfCode/2023/Day16/Contraption.cs
+++ src/AdventOfCode/2023/Day16/Contraption.cs
@@ -1,59 +1,45 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
+using AdventOfCode._2023.Day16.Tiles;
 
 namespace AdventOfCode._2023.Day16;
 
-public static class Contraption
+public class Contraption
 {
-    // follow the beam in the map and return the energized cell count. 
-    public static int EnergizedCells(Dictionary<Position, char> map, Beam startBeam)
+    private readonly Dictionary<Position, Tile> contraption;
+
+    public Contraption(IEnumerable<KeyValuePair<Position, Tile>> keyValuePairs)
+        => contraption = keyValuePairs.ToDictionary();
+
+    public int EnergizedTilesCount(Beam startingBeam)
     {
-        // this is essentially just a flood fill algorithm.
-        var q = new Queue<Beam>([startBeam]);
+        var queue = new Queue<Beam>([startingBeam]);
         var seen = new HashSet<Beam>();
 
-        while (q.TryDequeue(out var beam))
+        while (queue.TryDequeue(out var beam))
         {
             seen.Add(beam);
-            foreach (var dir in Tiles.Exits(map[beam.Position], beam.Direction))
+
+            var tile = contraption[beam.Position];
+            foreach (var bouncedBeam in beam.Bounce(tile))
             {
-                var pos = beam.Position + dir;
-                if (map.ContainsKey(pos) &&
-                    !seen.Contains(new Beam(pos, dir)))
+                if (contraption.ContainsKey(bouncedBeam.Position) &&
+                    !seen.Contains(bouncedBeam))
                 {
-                    q.Enqueue(new Beam(pos, dir));
+                    queue.Enqueue(bouncedBeam);
                 }
             }
         }
 
-        return seen.Select(energizedTile => energizedTile.Position).Distinct().Count();
+        return seen.Select(beam => beam.Position).Distinct().Count();
     }
 
-    // using a dictionary helps with bounds check (simply containskey):
-    public static Dictionary<Position, char> ParseContraption(string input)
-    {
-        var lines = input.Split('\n');
-        return (
-                   from irow in Enumerable.Range(0, lines.Length)
-                   from icol in Enumerable.Range(0, lines[0].Length)
-                   let tile = lines[irow][icol]
-                   let position = new Position(icol, irow)
-                   select new KeyValuePair<Position, char>(position, tile)
-               ).ToDictionary();
-    }
+    public Dictionary<Position, Tile>.KeyCollection Positions()
+        => contraption.Keys;
 
-    // go around the edges (top, right, bottom, left order) of the map
-    // and return the inward pointing directions
-    private static IEnumerable<Beam> StartBeams(Dictionary<Position, char> map)
-    {
-        var br = map.Keys.MaxBy(pos => pos.Imaginary + pos.Real);
-        return
-        [
-            ..from pos in map.Keys where pos.Real == 0 select new Beam(pos, Directions.Down),
-            ..from pos in map.Keys where pos.Real == br.Real select new Beam(pos, Directions.Left),
-            ..from pos in map.Keys where pos.Imaginary == br.Imaginary select new Beam(pos, Directions.Up),
-            ..from pos in map.Keys where pos.Imaginary == 0 select new Beam(pos, Directions.Right)
-        ];
-    }
+    public Position TopLeft()
+        => new(contraption.Keys.Min(pos => pos.Row), contraption.Keys.Min(pos => pos.Column));
+
+    public Position BottomRight()
+        => new(contraption.Keys.Max(pos => pos.Row), contraption.Keys.Max(pos => pos.Column));
 }
--- test/AdventOfCode.Tests/2023/Day16/PuzzleShould.cs
+++ test/AdventOfCode.Tests/2023/Day16/PuzzleShould.cs
@@ -1,6 +1,8 @@
-using System.Numerics;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
+using static AdventOfCode._2023.Day16.Beam;
+using static AdventOfCode._2023.Day16.ContraptionParser;
 
 namespace AdventOfCode._2023.Day16;
 
@@ -10,9 +12,25 @@ public class PuzzleShould
     [InputFileData("2023/Day16/sample.txt", 46)]
     [InputFileData("2023/Day16/input.txt", 6740)]
     public void Count_energized_tiles(string contraptionLayout, int energizedTiles)
-        => Contraption.EnergizedCells(
-                Contraption.ParseContraption(contraptionLayout),
-                new Beam(new Position(0,0), Directions.Right))
+    {
+        var contraption = Parse(contraptionLayout);
+        var startingBeam = new Beam(contraption.TopLeft(), Direction.Right);
+        contraption
+            .EnergizedTilesCount(startingBeam)
             .Should()
             .Be(energizedTiles);
+    }
+
+    [Theory]
+    [InputFileData("2023/Day16/sample.txt", 51)]
+    [InputFileData("2023/Day16/input.txt", 7041)]
+    public void Count_most_energized_starting_beam_tiles(string contraptionLayout, int energizedTiles)
+    {
+        var contraption = Parse(contraptionLayout);
+        StartingBeams(contraption)
+            .Select(startingBeam => contraption.EnergizedTilesCount(startingBeam))
+            .Max()
+            .Should()
+            .Be(energizedTiles);
+    }
 }
